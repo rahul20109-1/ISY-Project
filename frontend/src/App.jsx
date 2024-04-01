081360214a6ce50636fe49b293ac7abcbb0024f3
“RahulJaiswal_19” <rahuljaiswal.2019@outlook.com>
2024-04-01 16:06:12 +0530

    dashboard added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,7 @@ import Home from './pages/Home'
 import {BrowserRouter , Routes, Route} from 'react-router-dom'
 import Login from './pages/Login';
 import Register from './pages/Register';
+import DashboardPage from './pages/DashboardPage';
 const App = () => {
   return (
     <BrowserRouter>
@@ -10,6 +11,7 @@ const App = () => {
         <Route path='/' element={<Home/>} />
         <Route path='/login' element={<Login/>} />
         <Route path='/register' element={<Register/>} />
+        <Route path='/dashboard' element={<DashboardPage/>} />
       </Routes>
     </BrowserRouter>
   )
--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,18 +1,52 @@
-import React from 'react'
+import React,{useState} from 'react'
+import toast, { Toaster } from 'react-hot-toast';
 import {Link, useNavigate} from 'react-router-dom'
 const Login = () => {
+  const [formDataInfo, setFormDataInfo] = useState({});
+  const navigate = useNavigate();
+
+  const handleSubmit = async(e) => {
+    e.preventDefault();
+
+    try {
+      const res = await fetch('http://localhost:3000/api/user/login',{
+        method:"POST",
+        headers:{
+          "Content-Type":"application/json"
+        },
+        body:JSON.stringify(formDataInfo)
+      })
+      const data = await res.json();
+      if(res.ok){
+        toast.success(data.message);
+        navigate('/dashboard');
+      }
+      else{
+        toast.error(data.message);
+      }
+    } catch (error) {
+      toast.error(error);
+    }
+  }
+  const handleChange = (e) => {
+    setFormDataInfo({
+      ...formDataInfo,
+      [e.target.id]:e.target.value
+    })
+  }
   return (
     <div className='p-3 max-w-lg mx-auto space-y-5 mt-24'>
         <h1 className='text-3xl text-center'>Log in here</h1>
-        <form className='flex flex-col gap-3 mx-5'>
-          <input type="email" id='email' placeholder='john@example.com' required className='border border-black bg-slate-300 px-5 py-1 rounded-md text-black focus:outline-none' />
-          <input type="password" id='password' placeholder='*************' required className='border border-black bg-slate-300 px-5 py-1 rounded-md text-black focus:outline-none' />
+        <form className='flex flex-col gap-3 mx-5' onSubmit={handleSubmit}>
+          <input onChange={handleChange} type="email" id='email' placeholder='john@example.com' required className='border border-black bg-slate-300 px-5 py-1 rounded-md text-black focus:outline-none' />
+          <input onChange={handleChange} type="password" id='password' placeholder='*************' required className='border border-black bg-slate-300 px-5 py-1 rounded-md text-black focus:outline-none' />
           <div className='space-x-2 text-sm'>
             <span>Don't have an account?</span>
             <Link to={'/register'} className='text-blue-600 hover:underline'>Register</Link>
           </div>
-          <button className='bg-slate-600 text-white font-semibold px-3 py-2 hover:bg-slate-900 mx-44 rounded-full'>Log in</button>
+          <button type='submit' className='bg-slate-600 text-white font-semibold px-3 py-2 hover:bg-slate-900 mx-44 rounded-full'>Log in</button>
         </form>
+        <Toaster/>
     </div>
   )
 }

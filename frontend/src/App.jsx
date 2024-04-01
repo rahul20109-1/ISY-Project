77b8f9cbf11ec624f3d65e20e1c641cbec343c54
“RahulJaiswal_19” <rahuljaiswal.2019@outlook.com>
2024-04-01 12:35:38 +0530

    layout design finalized.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,17 @@
 import React from 'react'
-
+import Home from './pages/Home'
+import {BrowserRouter , Routes, Route} from 'react-router-dom'
+import Login from './pages/Login';
+import Register from './pages/Register';
 const App = () => {
   return (
-    <div>
-      App
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home/>} />
+        <Route path='/login' element={<Login/>} />
+        <Route path='/register' element={<Register/>} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 

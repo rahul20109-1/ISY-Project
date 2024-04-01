35d89bab94b5cf7f6d9e9d2f1b51a29164e75aa0
“RahulJaiswal_19” <rahuljaiswal.2019@outlook.com>
2024-04-01 13:02:50 +0530

    login UI done

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
-
+import {Link, useNavigate} from 'react-router-dom'
 const Login = () => {
   return (
-    <div>
-      LOGIN
+    <div className='p-3 max-w-lg mx-auto space-y-5 mt-24'>
+        <h1 className='text-3xl text-center'>Log in here</h1>
+        <form className='flex flex-col gap-3 mx-5'>
+          <input type="email" id='email' placeholder='john@example.com' required className='border border-black bg-slate-300 px-5 py-1 rounded-md text-black focus:outline-none' />
+          <input type="password" id='password' placeholder='*************' required className='border border-black bg-slate-300 px-5 py-1 rounded-md text-black focus:outline-none' />
+          <div className='space-x-2 text-sm'>
+            <span>Don't have an account?</span>
+            <Link to={'/register'} className='text-blue-600 hover:underline'>Register</Link>
+          </div>
+          <button className='bg-slate-600 text-white font-semibold px-3 py-2 hover:bg-slate-900 mx-44 rounded-full'>Log in</button>
+        </form>
     </div>
   )
 }

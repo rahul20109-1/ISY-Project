ce035a935733f53ff699b645ad43119dfbd990a9
“RahulJaiswal_19” <rahuljaiswal.2019@outlook.com>
2024-04-01 12:45:58 +0530

    header done.

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,10 +1,19 @@
 import React from 'react'
-
+import {useNavigate} from 'react-router-dom'
 const Header = () => {
+  const navigate = useNavigate();
+  const handleClick = () => {
+      navigate('/login')
+  }
   return (
-    <div>
-      Header
-    </div>
+    <header className='flex justify-between bg-slate-600 p-5'>
+      <div className='text-white font-medium text-xl'>
+        LOGO
+      </div>
+      <div>
+        <button onClick={handleClick} className='bg-blue-400 rounded-lg hover:bg-blue-600 px-3 py-1 text-sm text-white font-medium'>Log in</button>
+      </div>
+    </header>
   )
 }
 
